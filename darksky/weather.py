6eb2053309790cefa6f4f83f7b6c8e8d595d4319
ke-zhang-rd <kz2249@columbia.edu>
2018-08-14 10:25:06 -0400

    remove personal key

--- darksky/weather.py
+++ darksky/weather.py
@@ -3,8 +3,10 @@ import pprint
 import requests
 import time
 now = time.time()
-url = "https://api.darksky.net/forecast/b63d08d301bdd01da4c4f1218610372f/40.866610,-72.881760"
-text = requests.get(url.format(api_key='b63d08d301bdd01da4c4f1218610372f', lat=0, long=0, time=int(now))).text
+#url = "https://api.darksky.net/forecast/fs735892ewqerasdfefasdfef98dfe/40.866610,-72.881760"
+#text = requests.get(url.format(api_key='fs735892ewqerasdfefasdfef98dfe', lat=0, long=0, time=int(now))).text
+url = "https://api.darksky.net/forecast/{YOUR_SECREATE_KEY}/40.866610,-72.881760"
+text = requests.get(url.format(api_key='{YOUR_SECREATE_KEY}', lat=0, long=0, time=int(now))).text
 data = json.loads(text)
 
 pprint.pprint(data['currently']['apparentTemperature'])
